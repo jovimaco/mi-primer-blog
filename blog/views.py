4dc5c13cf373e8464ec277a1c11dabac76de90e3
JOVIMACO <josemariavillalbacortazzo@gmail.com>
2019-03-20 19:31:54 -0300

    Motrando datos de la bd al usuarios

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here
 
-def post_list(request):
-	return render(request, 'blog/post_list.html',{})
+def posts_list(request):
+	posts = Post.objects.filter(Published_date__lte=timezone.now()).order_by('pusblished_date')
+	return render(request, 'blog/post_list.html',{'posts':posts})
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('prueba/', include('blog.urls')),
 ]
